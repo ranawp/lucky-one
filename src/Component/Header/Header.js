ad7a975bf997b5228aef1df81c624bcf971116ef
ranawp <ranasaha4224#gmail.com>
2022-03-26 12:49:41 +0600

    title added

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="App">
+            <h1>Car house</h1>
+            <h4>Choose four cloths</h4>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Shop.css'
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="product-container">
+                <h1>Products</h1>
+            </div>
+            <div className="cart-container">
+                <h1>Cart</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
